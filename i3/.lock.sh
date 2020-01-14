68fd8fb60d6a4704795eb6b701ddc1799e18e058
Xavier Marques <xaviermarques4f@gmail.com>
2020-01-14 12:06:55 +0100

    Changed status bar i3

---
+++ i3/.lock.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+#   Script for locking the screen.
+#   Script is bound to a keyshortcut by i3 in its configuration file.
+#   
+#   REQUIRES: imagemagik, i3lock
+#################################################################################
+
+i3lock-fancy-dualmonitor
